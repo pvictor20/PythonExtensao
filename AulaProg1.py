b5ae2c94fa00a50993e3a9f2fc9582730861db87
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-03-22 11:13:24 -0300

    first commit

---
+++ AulaProg1.py
@@ -0,0 +1,21 @@
+print("Hello World")
+
+#Texto e número juntos
+print("Idade: ", 21)
+
+#Texto com booleano
+print("Cerveja:", True)
+
+#Textos longos com mais de uma linha
+print(''' Cardápio:
+    1 - Hamburguer
+    2- Dogão
+    3- Refrigerante
+ ''')
+
+ #Imprimir a mensagem
+ #"Sou incrivelmente muito bom", porém o muito deve se repetido várias vezes
+print("Eu sou ", 25*"muito " ,"bom")
+
+# -----#
+
---
+++ variaveis.py
@@ -0,0 +1,45 @@
+a = 2
+print(a)
+
+a = 3.14
+print(a)
+
+numero = 1000
+print("o numero digitado foi", numero)
+
+numero1 = 1
+numero2 = 2
+print("O numero {} é menor do que o numero {}" .format(numero1, numero2))
+
+#Declarar a variavel real
+
+pi = 3.14
+print(pi)
+
+texto1 = "eu sou incrível, "
+texto2 = "incrivelmente incrível."
+print(texto1 + texto2)
+
+
+#Declarar a varivale tipo lista
+
+lista = ['Batatinha', "Frita", 1, 2, 3]
+print(lista)
+
+produtos = ["PS5", "PS4", "PS3"]
+precos = [4800, 2500, 1200]
+print("{}: {}".format(produtos, precos))
+
+#Dicionario
+
+dic = {
+    "Nome do usuário" : 'Paulo Victor', 
+    'senha': 123,
+    'email': 'vito@vito.com'
+}
+
+print(dic)
+
+print("Digite seu nome: ")
+nome = input()
+print("Olá {}".format(nome))
