9ad4bec0bedd3161f4f155fc4ec0a9899803b246
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-03-29 08:58:12 -0300

    exercício 1 e 2

---
+++ Exercício1.py
@@ -0,0 +1,10 @@
+
+
+# 1) Fazer programa que converte velocidade km para ms
+
+print("Informe uma velocidade em m/s")
+velocidade_m_s = float(input())
+velocidade_km_h = velocidade_m_s / 3.6
+print("A velocidade de k/m em m/s é de: {} " .format(velocidade_km_h))
+
+
---
+++ Exercício2.py
@@ -0,0 +1,8 @@
+from math import pi
+
+# 2) Fazer um programa que o usuário digita o raio de uma circuferência 
+
+print("Informe o raio")
+raio = int(input())
+comprimento = 2 * pi * raio
+print("A circuferência é de: {}" .format(comprimento))
