f73572536af6b10b93d39cd67b5560e9b8cad486
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-03-29 10:36:03 -0300

    Exercício if else

---
+++ Condicional.py
@@ -0,0 +1,44 @@
+print("Condicional IF")
+
+n = 2
+if n == 2:
+    print("{} igual a 2".format(n))
+    print("boa")
+    
+print("\n")
+
+print("Condicional ELSE")
+n = 3
+if n == 2:
+    print("{} igual a 2".format(n))
+else:
+    print("{} diferente de 2".format(n))   
+
+print("\n") 
+
+print("Condicional ELIF")
+
+n = 7
+if n == 2:
+    print("{} é igual a 2".format(n))
+elif n == 3:
+    print("{} é igual a 3".format(n))
+else:
+    print("{} é diferente de 2 e de 3".format(n))
+
+
+print("\n")
+
+print("Exercício")
+
+x = 3
+y = 5
+
+if x == 10:
+    print("{} é maior que {}".format(y, x))
+else:
+    print("{} é maior que {}".format(y, x))
+
+
+
+
