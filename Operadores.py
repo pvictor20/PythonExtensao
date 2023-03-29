86feb7c91b1831485e6e768abedfa98564b4c722
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-03-29 09:42:42 -0300

    Exercício 5 e Operadores

---
+++ Operadores.py
@@ -0,0 +1,12 @@
+#Operadores maior > e menor <
+print(7 < 7)
+print( 7 > 5)
+print(7<=10)
+print(7 >= 7)
+print(7 >= 5)
+
+print("\n")
+
+#Operador de negação (não igual) !=
+print(7 != 7)
+print(7 != 5)
