1177e610ad0037fab7d181cd37ef5e750bfa3a7e
Paulo Victor <paulonascimento.7871@aluno.saojudas.br>
2023-03-29 10:01:20 -0300

    Operadores AND OR NOT e XOR

--- Operadores.py
+++ Operadores.py
@@ -10,3 +10,38 @@ print("\n")
 #Operador de negação (não igual) !=
 print(7 != 7)
 print(7 != 5)
+
+print("\n")
+
+print("PORTA AND")
+print("|   A\t|   B\t| SAÍDA |")
+print("|",False,"|",False,"|",False and False,"|")
+print("|",True,"\t|",False,"|",True and False,"|")
+print("|",False,"|",True,"\t|",False and True,"|")
+print("|",True,"\t|",True,"\t|",True and True,"\t|")
+
+print("\n")
+
+print("PORTA OR")
+print("|   A\t|   B\t| SAÍDA |")
+print("|",False,"|",False,"|",False or False,"|")
+print("|",True,"\t|",False,"|",True or False," |")
+print("|",False,"|",True,"\t|",False or True," |")
+print("|",True,"\t|",True,"\t|",True or True," |")
+
+print("\n")
+
+print("PORTA NOT")
+print("|   A\t| Não A |")
+print("|",False,"|",not False,"\t|")
+print("|",True,"\t|",not True,"|")
+
+print("\n")
+
+print("PORTA XOR")
+print("|   A\t|   B\t| SAÍDA |")
+print("|",False,"|",False,"|",False ^ False,"|")
+print("|",True,"\t|",False,"|",True ^ False,"\t|")
+print("|",False,"|",True,"\t|",False ^ True,"\t|")
+print("|",True,"\t|",True,"\t|",True ^ True,"|")
+
